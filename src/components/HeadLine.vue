61428f3cdd276f0eaa6525d736e4b06f1ec3f99c
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-06-18 23:50:50 +0900

    add headline changing the action title

---
+++ src/components/HeadLine.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <h1 class="font-bold tracking-tighter text-8xl mb-14">
+      <span :class="actionClasses">{{ action }} </span>
+      <br />
+      everyone
+    </h1>
+    <h2 class="text-3xl font-normal">Find your next job at Anonymous Careers</h2>
+  </section>
+</template>
+
+<script setup>
+import { onBeforeUnmount, ref, computed } from "vue";
+
+const action = ref("Build");
+const interval = ref(null);
+
+changeTitle();
+
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
+
+function changeTitle() {
+  const actions = ["Build", "Create", "Design", "Code"];
+  interval.value = setInterval(() => {
+    const currentActionIndex = actions.indexOf(action.value);
+    // use Modulo to cycle
+    const nextActionIndex = (currentActionIndex + 1) % 4; // 4 -> 0 so 1, 2, 3, 4->0, 1, 2, 3...
+    const nextAction = actions[nextActionIndex];
+    action.value = nextAction;
+  }, 3000);
+}
+
+const actionClasses = computed(() => {
+  // const className = action.value[0].toLowerCase() + action.value.slice(1) => no need
+  return {
+    [action.value.toLowerCase()]: true,
+  };
+});
+</script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,15 +1,17 @@
 <template>
   <main>
-    <section>
-      I can be your hero baby
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-700">1 col</div>
+        <div class="col-start-2 col-span-5 border border-blue-700"><HeadLine /></div>
+        <div class="col-start-7 col-span-5 border border-blue-700">5 col</div>
+        <div class="col-start-12 col-span-1 border border-blue-700">1 col</div>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup>
+import HeadLine from "@/components/HeadLine.vue"
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
