2ae17948ec517bf45fbd9a16afe01fe7c51ce2c4
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-09-09 23:35:48 +0900

    change to TS, JobFiltersSidebarOrganizations(unused)

---
+++ tests/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.ts
@@ -0,0 +1,84 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { mount } from '@vue/test-utils';
+import JobFiltersSidebarOrganizations from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue';
+
+import { setActivePinia, createPinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
+import { useJobsStore } from '@/store/store';
+import { useRouter } from 'vue-router';
+
+jest.mock('vue-router', () => ({
+  useRouter: jest.fn(),
+}));
+const mockUseRouter = useRouter as jest.Mock;
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+});
+
+describe('JobFiltersSidebarOrganizations', () => {
+  const createConfig = () => ({
+    global: {
+      stubs: {
+        AccordionContainer: false,
+        FontAwesomeIcon: true,
+      },
+      plugins: [createTestingPinia()],
+    },
+  });
+
+  it('renders unique list of organizations for filtering jobs', async () => {
+    const wrapper = mount(JobFiltersSidebarOrganizations, createConfig());
+    // after calling createTestingPinia(), we can configure store const jobsStore like below
+    const jobsStore = useJobsStore();
+    // @ts-expect-error: pinia getter is writable only in test // jobsStore.uniqueOrganization = Set(2) {"Google", "Amazon"};
+    jobsStore.uniqueOrganizations = new Set(['Google', 'Amazon']);
+    // console.log(initialJobsStore.uniqueOrganizations);
+    const clickableArea = wrapper.find("[data-test='clickable-area']");
+    await clickableArea.trigger('click');
+    // console.log(wrapper.html());
+    const organizationLabels = wrapper.findAll("[data-test='organization']");
+    const organizations = organizationLabels.map((node) => node.text());
+    expect(organizations).toEqual(['Google', 'Amazon']);
+    // set to undefined to reset the default behavior
+    // @ts-expect-error: pinia getter is writable only in test
+    jobsStore.uniqueOrganizations = undefined;
+  });
+
+  it('when check organization, selectedOrganizations will includes it', async () => {
+    const push = jest.fn();
+    mockUseRouter.mockImplementationOnce(() => ({
+      push,
+    }));
+    const wrapper = mount(JobFiltersSidebarOrganizations, createConfig());
+    const jobsStore = useJobsStore();
+    // @ts-expect-error: pinia getter is writable only in test
+    jobsStore.uniqueOrganizations = new Set(['Google', 'Amazon']);
+    const clickableArea = wrapper.find("[data-test='clickable-area']");
+    await clickableArea.trigger('click');
+    const googleInput = wrapper.find("[data-test='Google']");
+    // await googleInput.setChecked();
+    await googleInput.setValue(true);
+    expect(jobsStore.selectedOrganizations).toEqual(['Google']);
+  });
+
+  it('when check organization, go back to first page to see flesh filteredJobs', async () => {
+    const push = jest.fn();
+    mockUseRouter.mockImplementationOnce(() => ({
+      push,
+    }));
+    const wrapper = mount(JobFiltersSidebarOrganizations, createConfig());
+    const jobsStore = useJobsStore();
+    // @ts-expect-error: pinia getter is writable only in test
+    jobsStore.uniqueOrganizations = new Set(['Google', 'Amazon']);
+    const clickableArea = wrapper.find("[data-test='clickable-area']");
+    await clickableArea.trigger('click');
+    const googleInput = wrapper.find("[data-test='Google']");
+    // await googleInput.setChecked();
+    await googleInput.setValue(true);
+    expect(push).toHaveBeenCalledWith({ name: 'JobSearchResults' });
+  });
+});
